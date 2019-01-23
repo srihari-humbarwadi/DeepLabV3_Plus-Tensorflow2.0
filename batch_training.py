5cb412b758639416ae2583d6fec2e6d9763c3768
Srihari Humbarwadi <sriharihumbarwadi97@gmail.com>
2019-01-23 20:41:21 +0530

    random crops

--- batch_training.py
+++ batch_training.py
@@ -17,6 +17,7 @@ from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.losses import sparse_categorical_crossentropy, categorical_crossentropy
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import math
+from time import time
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 smooth = K.epsilon()
@@ -40,7 +41,7 @@ for i in range(len(image_list)):
 
 batch_size = int(sys.argv[1])
 samples = len(image_list)
-img_height, img_width = 512, 512
+img_height, img_width = 784, 784
 classes = 66
 
 
@@ -105,7 +106,7 @@ id_to_label = {v: k for v, k in enumerate(label_list)}
 
 
 def pipeline(image):
-    alpha = 0.5
+    alpha = 0.45
     dims = image.shape
     x = cv2.resize(image, (w, h))
     x = preprocess_input(np.float32(x))
@@ -115,6 +116,7 @@ def pipeline(image):
     y = np.argmax(z, axis=2)
 
     img_color = image.copy()
+    # img_color = cv2.resize(img_color, (w, h))
     for i in np.unique(y):
         img_color[y == i] = id_to_color[i]
     cv2.addWeighted(image, alpha, img_color, 1 - alpha, 0, img_color)
@@ -141,24 +143,25 @@ class TensorBoardImage(Callback):
         super().__init__()
         self.tag = tag
 
-    def on_epoch_end(self, epoch, logs={}):
-        print(f'viz running')
-        val_list = image_list[19500:]
-        i = [13, 355, 456, 132, 172, 200]
-        for idx in i:
-            print(f'*** saving viz {idx} ***')
-            test = load_img(f'{image_dir}/{val_list[idx]}')
-            test = img_to_array(test)
-            segmap = pipeline(test)
-            cv2.imwrite(f'train_outputs/{idx}_{epoch}.png',
-                        cv2.cvtColor(segmap, cv2.COLOR_RGB2BGR))
-
-            segmap = make_image(segmap)
-            summary = tf.Summary(
-                value=[tf.Summary.Value(tag=self.tag+f'_{i}', image=segmap)])
-            writer = tf.summary.FileWriter('./logs')
-            writer.add_summary(summary, epoch)
-            writer.close()
+    def on_batch_end(self, batch, logs={}):
+        if batch % 100 == 0:
+            print(f'viz running')
+            val_list = image_list[19500:]
+            i = [13, 355, 456, 132, 172, 200]
+            summaries = []
+            for idx in i:
+                print(f'*** saving viz {idx} ***')
+                test = load_img(f'{image_dir}/{val_list[idx]}')
+                test = img_to_array(test)
+                segmap = pipeline(test)
+                cv2.imwrite(f'train_outputs/{idx}_{time()}.png',
+                            cv2.cvtColor(segmap, cv2.COLOR_RGB2BGR))
+                segmap = make_image(segmap)
+                # summaries.append(tf.Summary.Value(tag=self.tag, image=segmap))
+                summary = tf.Summary(value=[tf.Summary.Value(tag=self.tag + f'_{i}', image=segmap)])
+                writer = tf.summary.FileWriter('logs')
+                writer.add_summary(summary, batch)
+                writer.close()
 
 
 class seg_gen(Sequence):
@@ -179,8 +182,7 @@ class seg_gen(Sequence):
     #         _image = preprocess_input(_image)
     #         mask = image.img_to_array(image.load_img(
     #             f'{mask_dir}/{mask_list[i]}', color_mode='grayscale', target_size=(img_height, img_width)))
-    #         mask = to_categorical(mask, num_classes=classes)
-
+    #         # mask = to_categorical(mask, num_classes=classes)
     #         batch_y.append(mask)
     #         batch_x.append(_image)
     #     return np.array(batch_x), np.array(batch_y)
@@ -192,29 +194,30 @@ class seg_gen(Sequence):
             _image = image.img_to_array(image.load_img(
                 f'{image_dir}/{image_list[i]}'))
             _image = preprocess_input(_image)
-            mask = image.img_to_array(image.load_img(
+            _mask = image.img_to_array(image.load_img(
                 f'{mask_dir}/{mask_list[i]}', color_mode='grayscale'))
 
-            hh = _image.shape[0]
-            ww = _image.shape[1]
+            ih = _image.shape[0]
+            iw = _image.shape[1]
 
-            maxh = np.random.randint(img_height, hh)
-            maxw = maxh
+            maxd = min(ih, iw)
+            maxd -= 1
+            # print(img_width, maxd, _image.shape)
+            rndmd = np.random.randint(img_height, maxd)
 
-            rnh = np.random.randint(0, hh - maxh)
-            rnw = np.random.randint(0, hh - maxw)
+            rh = np.random.randint(0, ih - rndmd)
+            rw = np.random.randint(0, ih - rndmd)
 
-            _image = _image[rnh:rnh + maxh, rnw:rnw + maxw, :]
-            mask = mask[rnh:rnh + maxh, rnw:rnw + maxw, :]
+            _image_ = _image[rh:rh + rndmd, rw:rw + rndmd, :]
+            _mask_ = _mask[rh:rh + rndmd, rw:rw + rndmd, :]
 
-            _image = cv2.resize(_image, (img_width, img_height))
-            mask = cv2.resize(mask, (img_width, img_height))
-            # mask = mask.reshape(img_height, img_width, 1)
-            mask = to_categorical(mask, num_classes=66)
-            batch_y.append(mask)
-            batch_x.append(_image)
-            del mask
-            del _image
+            _image_ = cv2.resize(_image_, (img_width, img_height))
+            _mask_ = cv2.resize(_mask_, (img_width, img_height))
+            _mask_ = np.expand_dims(_mask_, axis=-1)
+
+            # mask = to_categorical(mask, num_classes=classes)
+            batch_y.append(_mask_)
+            batch_x.append(_image_)
         return np.array(batch_x), np.array(batch_y)
 
 
@@ -237,7 +240,7 @@ model = DeepLabV3Plus(img_height, img_width, classes)
 print('*** Building multi_gpu_model ***')
 pmodel = multi_gpu_model(model, 4)
 pmodel.compile(optimizer=Adam(lr=1e-4),
-               loss=ce_dice_loss, metrics=['acc'])
+               loss='sparse_categorical_crossentropy', metrics=['acc'])
 print('*** Building multi_gpu_model completed ***')
 
 
@@ -245,7 +248,7 @@ tb = TensorBoard(log_dir='logs', write_graph=True)
 mc = ModelCheckpoint(mode='max', filepath='top_weights.h5', monitor='val_acc',
                      save_best_only='True', save_weights_only='True', verbose=1)
 lrsched = LearningRateScheduler(lr_sched)
-viz = TensorBoardImage('validation_image')
+viz = TensorBoardImage('validation_images')
 callbacks = [mc, viz, tb]
 
 train_gen = seg_gen(image_list[:-500], mask_list[:-500], batch_size, 'train')
