fixed image normalization

batch_training.py
@@ -13,7 +13,6 @@ from tensorflow.python.keras.callbacks import TensorBoard, ModelCheckpoint, Earl
 from tensorflow.python.keras.preprocessing import image
 from tensorflow.python.keras.utils import Sequence
 from deeplab import DeepLabV3Plus
-from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.losses import sparse_categorical_crossentropy, categorical_crossentropy
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import math
@@ -45,17 +44,6 @@ img_height, img_width = 784, 784
 classes = 66
 
 
-# def dice_coef_multilabel(y_true, y_pred, n=66):
-#     def dice_coef(y_true, y_pred):
-#         y_true_f = K.flatten(y_true)
-#         y_pred_f = K.flatten(y_pred)
-#         intersection = K.sum(y_true_f * y_pred_f)
-#         return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-#     dice = 0
-#     for index in range(n):
-#         dice -= dice_coef(y_true[:, :, :, index], y_pred[:, :, :, index])
-#     return dice
-
 
 def jaccard_coef(y_true, y_pred):
     y_true_f = K.flatten(y_true)
@@ -66,31 +54,11 @@ def jaccard_coef(y_true, y_pred):
     return 1 - J
 
 
-def ce_dice_loss(y_true, y_pred, n=66):
+def ce_jaccard_loss(y_true, y_pred, n=66):
     for index in range(n):
         J = 0
         J += jaccard_coef(y_true[:, :, :, index], y_pred[:, :, :, index])
     return categorical_crossentropy(y_true, y_pred) + J
-
-# def focal_loss1(target, output, gamma=2):
-#     output /= K.sum(output, axis=-1, keepdims=True)
-#     eps = K.epsilon()
-#     output = K.clip(output, eps, 1. - eps)
-#     return -K.sum(K.pow(1. - output, gamma) * target * K.log(output), axis=-1)
-
-
-# def focal_loss(y_true, y_pred):
-#     gamma = 2.
-#     alpha = 4.
-#     epsilon = 1.e-9
-#     y_true = tf.convert_to_tensor(y_true, tf.float32)
-#     y_pred = tf.convert_to_tensor(y_pred, tf.float32)
-#     model_out = tf.add(y_pred, epsilon)
-#     ce = tf.multiply(y_true, -tf.log(model_out))
-#     weight = tf.multiply(y_true, tf.pow(tf.subtract(1., model_out), gamma))
-#     fl = tf.multiply(alpha, tf.multiply(weight, ce))
-#     reduced_fl = tf.reduce_max(fl, axis=1)
-#     return tf.reduce_mean(reduced_fl)
 
 
 h, w = img_height, img_width
@@ -150,14 +118,12 @@ class TensorBoardImage(Callback):
             i = [13, 355, 456, 132, 172, 200]
             summaries = []
             for idx in i:
-                print(f'*** saving viz {idx} ***')
                 test = load_img(f'{image_dir}/{val_list[idx]}')
                 test = img_to_array(test)
                 segmap = pipeline(test)
                 cv2.imwrite(f'train_outputs/{idx}_{time()}.png',
                             cv2.cvtColor(segmap, cv2.COLOR_RGB2BGR))
                 segmap = make_image(segmap)
-                # summaries.append(tf.Summary.Value(tag=self.tag, image=segmap))
                 summary = tf.Summary(value=[tf.Summary.Value(tag=self.tag + f'_{i}', image=segmap)])
                 writer = tf.summary.FileWriter('logs')
                 writer.add_summary(summary, batch)
@@ -165,60 +131,55 @@ class TensorBoardImage(Callback):
 
 
 class seg_gen(Sequence):
-    def __init__(self, x_set, y_set, batch_size, name):
+    def __init__(self, x_set, y_set, batch_size):
         self.x, self.y = x_set, y_set
         self.batch_size = batch_size
-        print(f'*** Initializating {name} generator ***')
 
     def __len__(self):
         return int(np.ceil(len(self.x) / float(self.batch_size)))
 
-    # def __getitem__(self, idx):
-    #     idx = np.random.randint(0, samples, batch_size)
-    #     batch_x, batch_y = [], []
-    #     for i in idx:
-    #         _image = image.img_to_array(image.load_img(
-    #             f'{image_dir}/{image_list[i]}', target_size=(img_height, img_width)))
-    #         _image = preprocess_input(_image)
-    #         mask = image.img_to_array(image.load_img(
-    #             f'{mask_dir}/{mask_list[i]}', color_mode='grayscale', target_size=(img_height, img_width)))
-    #         # mask = to_categorical(mask, num_classes=classes)
-    #         batch_y.append(mask)
-    #         batch_x.append(_image)
-    #     return np.array(batch_x), np.array(batch_y)
-
     def __getitem__(self, idx):
-        idx = np.random.randint(0, samples, batch_size)
+        idx = np.random.randint(0, len(self.x), batch_size)
         batch_x, batch_y = [], []
         for i in idx:
-            _image = image.img_to_array(image.load_img(
-                f'{image_dir}/{image_list[i]}'))
-            _image = preprocess_input(_image)
-            _mask = image.img_to_array(image.load_img(
-                f'{mask_dir}/{mask_list[i]}', color_mode='grayscale'))
-
-            ih = _image.shape[0]
-            iw = _image.shape[1]
-
-            maxd = min(ih, iw)
-            maxd -= 1
-            # print(img_width, maxd, _image.shape)
-            rndmd = np.random.randint(img_height, maxd)
-
-            rh = np.random.randint(0, ih - rndmd)
-            rw = np.random.randint(0, ih - rndmd)
-
-            _image_ = _image[rh:rh + rndmd, rw:rw + rndmd, :]
-            _mask_ = _mask[rh:rh + rndmd, rw:rw + rndmd, :]
-
-            _image_ = cv2.resize(_image_, (img_width, img_height))
-            _mask_ = cv2.resize(_mask_, (img_width, img_height))
-            _mask_ = np.expand_dims(_mask_, axis=-1)
-
-            # mask = to_categorical(mask, num_classes=classes)
-            batch_y.append(_mask_)
-            batch_x.append(_image_)
+            _image = image.img_to_array(image.load_img(f'{image_dir}/{image_list[i]}', target_size=(img_height, img_width)))/255.   
+            mask = image.img_to_array(image.load_img(f'{mask_dir}/{mask_list[i]}', grayscale=True, target_size=(img_height, img_width)))
+            batch_y.append(mask)
+            batch_x.append(_image)
         return np.array(batch_x), np.array(batch_y)
+# to-do
+-> data augmentation on random crops
+#     def __getitem__(self, idx):
+#         idx = np.random.randint(0, samples, batch_size)
+#         batch_x, batch_y = [], []
+#         for i in idx:
+#             _image = image.img_to_array(image.load_img(
+#                 f'{image_dir}/{image_list[i]}'))
+#             _image = preprocess_input(_image)
+#             _mask = image.img_to_array(image.load_img(
+#                 f'{mask_dir}/{mask_list[i]}', color_mode='grayscale'))
+
+#             ih = _image.shape[0]
+#             iw = _image.shape[1]
+
+#             maxd = min(ih, iw)
+#             maxd -= 1
+#             # print(img_width, maxd, _image.shape)
+#             rndmd = np.random.randint(img_height, maxd)
+
+#             rh = np.random.randint(0, ih - rndmd)
+#             rw = np.random.randint(0, ih - rndmd)
+
+#             _image_ = _image[rh:rh + rndmd, rw:rw + rndmd, :]
+#             _mask_ = _mask[rh:rh + rndmd, rw:rw + rndmd, :]
+
+#             _image_ = cv2.resize(_image_, (img_width, img_height))
+#             _mask_ = cv2.resize(_mask_, (img_width, img_height))
+#             _mask_ = np.expand_dims(_mask_, axis=-1)
+
+#             batch_y.append(_mask_)
+#             batch_x.append(_image_/255.)
+#         return np.array(batch_x), np.array(batch_y)
 
 
 def lr_sched(epoch):
@@ -232,15 +193,15 @@ def lr_sched(epoch):
 
 
 model = DeepLabV3Plus(img_height, img_width, classes)
-# try:
-#     print('Loading previous weights')
-#     model.load_weights('model.h5')
-# except:
-#     print('Training from scratch !!!')
+try:
+    print('Loading previous weights')
+    model.load_weights('top_weights.h5')
+except:
+    print('Training from scratch !!!')
 print('*** Building multi_gpu_model ***')
 pmodel = multi_gpu_model(model, 4)
 pmodel.compile(optimizer=Adam(lr=1e-4),
-               loss='sparse_categorical_crossentropy', metrics=['acc'])
+               loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 print('*** Building multi_gpu_model completed ***')
 
 
@@ -251,8 +212,8 @@ lrsched = LearningRateScheduler(lr_sched)
 viz = TensorBoardImage('validation_images')
 callbacks = [mc, viz, tb]
 
-train_gen = seg_gen(image_list[:-500], mask_list[:-500], batch_size, 'train')
-val_gen = seg_gen(image_list[-500:], mask_list[-500:], batch_size, 'val')
+train_gen = seg_gen(image_list[:-500], mask_list[:-500], batch_size)
+val_gen = seg_gen(image_list[-500:], mask_list[-500:], batch_size)
 steps = train_gen.__len__()
 val_steps = val_gen.__len__()
 
@@ -274,7 +235,8 @@ print(f'*** Val steps = {val_steps} ***')
 
 
 pmodel.fit_generator(train_gen, validation_data=val_gen, validation_steps=val_steps, steps_per_epoch=steps, epochs=int(sys.argv[2]),
-                     callbacks=callbacks, workers=8, use_multiprocessing=True, max_queue_size=50)
+                     callbacks=callbacks, workers=8, use_multiprocessing=True, max_queue_size=250)
 # pmodel.fit(batch_x, batch_y, batch_size=batch_size, epochs=int(sys.argv[2]), validation_split=0.1, callbacks=callbacks)
 print('Saving final weights')
+pmodel.load_weights('top_weights.h5')
 model.save_weights('model.h5')